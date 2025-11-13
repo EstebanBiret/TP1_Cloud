d71e4e614b8746b341c3d52bcc24b50e22b96b00
Esteban BIRET-TOSCANO <100210730+EstebanBiret@users.noreply.github.com>
2025-11-13 09:23:20 +0100

    feat: site-web-statique

--- main.tf
+++ main.tf
@@ -1,17 +1,59 @@
 terraform {
-  required_providers {
-    minio = {
-      source  = "terraform-provider-minio/minio"
-      version = ">= 3.1.0"
+    required_providers {
+        minio = {
+            source  = "terraform-provider-minio/minio"
+            version = ">= 3.1.0"
+        }
     }
-  }
 }
+
 provider "minio" {
-  minio_server   = "127.0.0.1:9000"
-  minio_user     = "minioadmin"
-  minio_password = "minioadmin"
+    minio_server   = "127.0.0.1:9000"
+    minio_user     = "minioadmin"
+    minio_password = "minioadmin"
 }
+
 resource "minio_s3_bucket" "tp1_bucket" {
   bucket = "tp1-cloud-bucket"
   acl    = "private"
+}
+
+resource "minio_s3_bucket" "web_bucket" {
+    bucket = "webbucket"
+    acl = "public-read"
+}
+resource "minio_s3_object" "index_html" {
+    bucket_name = minio_s3_bucket.web_bucket.bucket
+    object_name = "index.html"
+    source = "index.html"
+    content_type = "text/html"
+
+}
+resource "minio_s3_object" "style_css" {
+    bucket_name = minio_s3_bucket.web_bucket.bucket
+    object_name = "style.css"
+    source = "style.css"
+    content_type = "text/css"
+}
+
+output "website_url" {
+    value = "http://${minio_s3_bucket.web_bucket.bucket}.localhost:9000"
+}
+
+resource "minio_s3_bucket_policy" "webbucket_policy" {
+  bucket = minio_s3_bucket.web_bucket.bucket
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": ["s3:GetObject"],
+      "Resource": ["arn:aws:s3:::webbucket/*"]
+    }
+  ]
+}
+EOF
 }
